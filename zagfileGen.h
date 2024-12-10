0f9776995658bfa6e4dce3538d033edc3d7a27be
Dasha <kuzdashad@gmail.com>
2024-12-10 19:58:38 +0500

    заголовочный файл

---
+++ zagfileGen.h
@@ -0,0 +1,10 @@
+#ifndef ZAGFILEGEN_H
+#define ZAGFILEGEN_H
+
+void Gen(int& number1, int& number2);
+int question(int& number1, int& number2);
+int Proverka(int& number1, int& number2);
+int Ball(int& number1, int& number2, int& summ);
+int Mark(int& summ, int& questions);
+
+#endif
